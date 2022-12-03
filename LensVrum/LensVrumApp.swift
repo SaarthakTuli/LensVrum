acb02ebaf1385fcdad674cfa38ee67c9cdabb911
SaarthakTuli <saarthaktuli@gmail.com>
2022-12-03 20:11:42 +0530

    Initial Commit

---
+++ LensVrum/LensVrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LensVrumApp.swift
+//  LensVrum
+//
+//  Created by Saarthak Tuli on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LensVrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
